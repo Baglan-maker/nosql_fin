76dae78a405e548a64794298bceb7b56e43f7543
Baglan-maker <bagl203456@gmail.com>
2025-02-25 06:16:33 +0500

    final

--- backend/api/index.js
+++ backend/api/index.js
@@ -20,16 +20,16 @@ mongoose.connect('mongodb+srv://bagl203456:xTevqEP8XWPEYsG8@cluster0.jvw05.mongo
   .catch(err => console.error(err));
 
 // Подключаем маршруты
-const bookRoutes = require('./routes/bookRoutes');
+const bookRoutes = require('../routes/bookRoutes');
 app.use('/api/books', bookRoutes);
 
-const reviewRoutes = require('./routes/reviewRoutes');
+const reviewRoutes = require('../routes/reviewRoutes');
 app.use('/api/reviews', reviewRoutes);
 
-const userRoutes = require('./routes/userRoutes');
+const userRoutes = require('../routes/userRoutes');
 app.use('/api/users', userRoutes);
 
-const analyticsRoutes = require('./routes/analyticsRoutes');
+const analyticsRoutes = require('../routes/analyticsRoutes');
 app.use('/api/analytics', analyticsRoutes);
 
 app.get('*', (req, res) => {
